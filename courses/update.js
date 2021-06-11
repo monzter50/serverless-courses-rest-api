97fbc2e1b7f150bbe486d609dce3745583777bb0
Jose Gonzalez <josemonzter50@gmail.com>
2021-06-11 08:48:17 -0500

    feat: configure my cruds services

---
+++ courses/update.js
@@ -0,0 +1,59 @@
+'use strict';
+
+const AWS = require('aws-sdk'); // eslint-disable-line import/no-extraneous-dependencies
+
+const dynamoDb = new AWS.DynamoDB.DocumentClient();
+
+module.exports.update = (event, context, callback) => {
+  const timestamp = new Date().getTime();
+  const data = JSON.parse(event.body);
+
+  // validation
+  if (typeof data.title !== 'string' || typeof data.published !== 'boolean') {
+    console.error('Validation Failed');
+    callback(null, {
+      statusCode: 400,
+      headers: { 'Content-Type': 'text/plain' },
+      body: 'Couldn\'t update the courses item.',
+    });
+    return;
+  }
+
+  const params = {
+    TableName: process.env.DYNAMODB_TABLE,
+    Key: {
+      id: event.pathParameters.id,
+    },
+    ExpressionAttributeNames: {
+      '#courses_title': 'title',
+    },
+    ExpressionAttributeValues: {
+      ':title': data.title,
+      ':published': data.published,
+      ':updatedAt': timestamp,
+    },
+    UpdateExpression: 'SET #courses_title = :title, published = :published, updatedAt = :updatedAt',
+    ReturnValues: 'ALL_NEW',
+  };
+
+  // update the todo in the database
+  dynamoDb.update(params, (error, result) => {
+    // handle potential errors
+    if (error) {
+      console.error(error);
+      callback(null, {
+        statusCode: error.statusCode || 501,
+        headers: { 'Content-Type': 'text/plain' },
+        body: 'Couldn\'t fetch the todo item.',
+      });
+      return;
+    }
+
+    // create a response
+    const response = {
+      statusCode: 200,
+      body: JSON.stringify(result.Attributes),
+    };
+    callback(null, response);
+  });
+};
